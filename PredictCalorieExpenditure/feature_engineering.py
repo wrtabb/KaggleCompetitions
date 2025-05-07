09a444aaf498042dbdca4138e696a079a5dacfa0
wrtabb <wrtabb@gmail.com>
2025-05-07 13:47:27 -0700

    updated feature_engineering.py to save new data over old data

--- PredictCalorieExpenditure/feature_engineering.py
+++ PredictCalorieExpenditure/feature_engineering.py
@@ -16,3 +16,5 @@ print(com_data.head())
 cf.plot_feature_distributions([com_data,tar_data],'added_features')
 cf.plot_MI(com_data[:len(tar_data)],tar_data,'added_features')
 cf.plot_correlations(com_data,'added_features')
+
+com_data.to_csv(f'{data_loc}/combo_data.csv',index=False)
